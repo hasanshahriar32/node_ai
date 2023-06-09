979da1b8ac45a1a75d940c815f5e65305abe9420
Mr. Hasan <hasanshahriar32@gmail.com>
2023-06-09 18:54:33 +0600

    new node ai created

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+const port = process.env.port || 5000;
+app.listen(port, console.log("app is listening to port ", port));
